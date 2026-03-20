8bdf0bf6a86ca5ce4c59d911784a24bba64852c2
Vlad <v.jurca@student.utwente.nl>
2026-03-20 17:32:53 +0100

    comments

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from collections import Counter
 
 from nltk.corpus import stopwords
+from scipy.odr import exponential
+
 nltk.download('punkt_tab')
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
@@ -19,6 +21,9 @@ with open(file_name, "r", encoding="utf-8", errors="ignore") as file:
     cleaned_text = content.replace("\n", " ")
 
 data = []
+
+# we get rid of stop words because they provide no unique context
+# and the model can focus  on more meaningful word relationships
 for i in sent_tokenize(cleaned_text):
     temp_words = []
     # tokenize the sentence into words
@@ -40,10 +45,12 @@ id_to_word = {i: word for i, word in enumerate(vocab)}
 new_filtered_data = [[word for word in sentence if word in word_to_id] for sentence in data]
 new_filtered_data = [s for s in new_filtered_data if len(s) > 1]
 
+
 window_size = 5
 X = []
 Y = []
 
+# create pairs with 5 words from the left and 5 words from the right of a specific word
 for sentence in new_filtered_data:
     word_ids = [word_to_id[word] for word in sentence]
 
@@ -61,12 +68,14 @@ for sentence in new_filtered_data:
             Y.append(context_id)
 
 
+
 input_words = np.array(X, dtype=np.int32)
 target_words = np.array(Y, dtype=np.int32)
 
 input_w = []
 output_w = []
-
+# define the number of dimensions we want to use, also the number of
+# columns in the input weight
 embedding_size = 50
 
 for i in range(len(vocab)):
@@ -79,7 +88,6 @@ for i in range(len(vocab)):
 
         word_values_input.append(x)
         word_values_output.append(y)
-
         j += 1
 
     input_w.append(word_values_input)
@@ -105,24 +113,26 @@ for epoch in range(epochs):
 
         # apply softmax function to get the probability of each word
         # appearing near the given input word
-        expected_score = np.exp(score - np.max(score))
-        y_pred = expected_score / np.sum(expected_score)
+        exponential_score = np.exp(score - np.max(score))
+        softmax = exponential_score / np.sum(exponential_score)
 
         #calculate the error that the model made
-        error = y_pred.copy()
+        error = softmax.copy()
         error[context_id] -= 1
 
+        # backpropagation
         # calculate the gradients using the outer product function in numpy
         output_gradient = np.outer(error, target_word_vector)
         input_gradient = np.dot(output_weights.T, error)
 
+        # update the weights
         output_weights -= step_size * output_gradient
         input_weights[target_id] -= step_size * input_gradient
-        total_loss -= np.log(y_pred[context_id])
+        total_loss -= np.log(softmax[context_id])
     print(f"Epoch {epoch + 1}/{epochs}, Loss: {total_loss}")
 
 
-def get_neighbors(target_word, top_n=10):
+def get_neighbors(target_word, n=10):
     if target_word not in word_to_id:
         return f"'{target_word}' not in vocabulary."
 
@@ -134,12 +144,13 @@ def get_neighbors(target_word, top_n=10):
         v_b = input_weights[i]
         norm_a = np.linalg.norm(v_a)
         norm_b = np.linalg.norm(v_b)
+        # apply the cosine similarity formula
         score1 = np.dot(v_a, v_b) / (norm_a * norm_b)
         similarities.append((id_to_word[i], score1))
 
     similarities.sort(key=lambda x: x[1], reverse=True)
 
-    return similarities[1:top_n + 1]
+    return similarities[1:n + 1]
 
 
 
